67c22c9e6965b6b32e0d1eef0cf593dc4d78539a
Hoopes <matthew.hoopes@gmail.com>
2020-11-15 20:34:44 -0500

    Initial Commit

---
+++ AK5Test1/AK5Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  AK5Test1App.swift
+//  AK5Test1
+//
+//  Created by Matthew Hoopes on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct AK5Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
